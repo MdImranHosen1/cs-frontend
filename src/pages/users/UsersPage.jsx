7a5208ecae07f75de7aef423b9dc56eb3b1bf2ca
MdImranHosen1 <www.imransk.com@gmail.com>
2024-03-28 14:35:05 +0600

    "User card desing fix"

--- src/pages/users/UsersPage.jsx
+++ src/pages/users/UsersPage.jsx
@@ -15,11 +15,13 @@ export const UsersPage = () => {
         <UserFrom />
       </div>
       <div className=" w-3/4 p-5">
-        <div className=" w-full mb-5">
-          {users.map((value) => {
-            return <UserCard users={value} />;
-          })}
-        </div>
+        {users.map((value) => {
+          return (
+            <div className=" w-full mb-1 pr-3">
+              <UserCard users={value} />;
+            </div>
+          );
+        })}
       </div>
     </div>
   );
