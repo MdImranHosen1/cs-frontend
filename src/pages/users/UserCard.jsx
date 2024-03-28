14fb7112bfc5f492c229716a4f757f3e8a6e66e5
MdImranHosen1 <www.imransk.com@gmail.com>
2024-03-28 17:34:54 +0600

    "User final "

--- src/pages/users/UserCard.jsx
+++ src/pages/users/UserCard.jsx
@@ -1,19 +1,17 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
-export default function UserCard() {
+export default function UserCard({users}) {
+  
   return (
     <>
       <div class=" w-full p-6 bg-white border border-gray-300 rounded-lg shadow ">
         <Link to="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-            Noteworthy technology acquisitions 2021
+            {users.userName}
           </h5>
         </Link>
-        <p class="mb-3 font-normal text-gray-700 ">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
+        <p class="mb-3 font-normal text-gray-700 ">{users.userPhone}</p>
         <Link
           to="#"
           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
